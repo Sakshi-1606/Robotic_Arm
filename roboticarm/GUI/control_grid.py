46c0f30f892c1a13a9915ac613fd264108c22617
JoelJojoP <joeljojop@gmail.com>
2023-05-06 19:22:48 +0530

    Fixed slider limits

--- roboticarm/GUI/control_grid.py
+++ roboticarm/GUI/control_grid.py
@@ -6,12 +6,12 @@ from std_msgs.msg import Float64
 import tkinter as tk
 
 class SliderController:
-    def __init__(self, master, name, topic):
+    def __init__(self, master, name, topic, range):
         # Create a publisher to publish the slider value to the joint controller
         self.pub = rospy.Publisher(topic, Float64, queue_size=10)
-
+        rate = rospy.Rate(10)
         # Create a horizontal slider
-        self.slider = tk.Scale(master, from_=0, to=360, orient=tk.HORIZONTAL, length=200, width=20, sliderlength=10, bg="black", command=self.slider_callback)
+        self.slider = tk.Scale(master, from_=range[0], to=range[1], orient=tk.HORIZONTAL, length=200, width=20, sliderlength=10, bg="blue", command=self.slider_callback)
         self.slider.grid(row=0, column=0, columnspan=1, pady=5)
 
         # Create a label with the controller name
@@ -23,7 +23,7 @@ class SliderController:
         angle = float(value) * 3.14159 / 180.0
 
         # Publish the angle to the joint controller
-        self.pub.publish(Float64(angle))
+        self.pub.publish(Float64(angle))    
 
 class RoboticArmController:
     def __init__(self):
@@ -35,19 +35,24 @@ class RoboticArmController:
         self.root.title("Robotic Arm Controller")
 
         # Set the padding between rows and columns
-        pad = 100
+        pad = 10
 
         # Create a 3x3 grid of slider controllers
-        for row in range(3):
-            for col in range(3):
-                # Calculate the controller name and topic based on the row and column
-                name = f"Controller{row*3+col+1}"
-                topic = f"/roboticarm/joint{row*3+col+1}_position_controller/command"
-                controller = SliderController(self.root, name, topic)
+        for row in range(5):
+            # Calculate the controller name and topic based on the row and column
+            name = f"Controller {row+1}"
+            topic = f"/roboticarm/joint{row+1}_position_controller/command"
+            controller = None
+            if row==0 or row==4:
+                controller = SliderController(self.root, name, topic,[-180,180])
+            elif row==1:
+                controller = SliderController(self.root, name, topic, [-90,90])
+            elif row==2 or row==3:
+                controller = SliderController(self.root, name, topic, [-97.4 ,97.4])
 
-                # Add the controller to the grid with padding
-                controller.label.grid(row=row+1, column=col*2, padx=pad, pady=pad, sticky="E")
-                controller.slider.grid(row=row+1, column=col*2+1, padx=pad, pady=pad, sticky="W")
+            # Add the controller to the grid with padding
+            controller.label.grid(row=row+1, column=1, padx=pad, pady=pad, sticky="E")
+            controller.slider.grid(row=row+1, column=2, padx=pad, pady=pad, sticky="W")
 
         # Set the padding around window
         self.root.geometry(f"+{pad}+{pad}")
@@ -58,4 +63,7 @@ class RoboticArmController:
 
 if __name__ == '__main__':
     controller = RoboticArmController()
-    controller.run()
+    try:
+        controller.run()
+    except rospy.ROSInterruptException:
+        pass
