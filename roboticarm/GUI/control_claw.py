5c705a729802dcd3b92c4cf6c213c6b52057c973
JoelJojoP <joeljojop@gmail.com>
2023-05-27 20:12:35 +0530

    Added final control claw code

--- roboticarm/GUI/control_claw.py
+++ roboticarm/GUI/control_claw.py
@@ -26,6 +26,34 @@ class SliderController:
         # Publish the angle to the joint controller
         self.pub.publish(Float64(angle))
 
+
+class ClawController:
+    def __init__(self, master, name, open_value, close_value):
+        # Create a publisher to publish the claw value to the joint controller
+        self.pub1 = rospy.Publisher('/roboticarm/joint6_position_controller/command', Float64, queue_size=10)
+        self.pub2 = rospy.Publisher('/roboticarm/joint7_position_controller/command', Float64, queue_size=10)
+        self.pub3 = rospy.Publisher('/roboticarm/joint8_position_controller/command', Float64, queue_size=10)
+        self.pub4 = rospy.Publisher('/roboticarm/joint9_position_controller/command', Float64, queue_size=10)
+
+        # Create an open and close button
+        self.open_button = tk.Button(master, text="Open", command=lambda: self.button_callback(open_value))
+        self.open_button.pack(padx=5)
+
+        self.close_button = tk.Button(master, text="Close", command=lambda: self.button_callback(close_value))
+        self.close_button.pack(padx=5)
+
+        # Create a label with the controller name
+        self.label = tk.Label(master, text=name)
+        self.label.pack()
+
+    def button_callback(self, value):
+        # Publish the claw value to the joint controller
+        self.pub1.publish(Float64(-1*value))
+        self.pub2.publish(Float64(value))
+        self.pub3.publish(Float64(-1*value))
+        self.pub4.publish(Float64(value))
+
+
 class RoboticArmController:
     def __init__(self):
         # Initialize ROS node
@@ -36,10 +64,17 @@ class RoboticArmController:
         self.root.title("Robotic Arm Controller")
 
         # Set the padding around window
-        self.root.geometry("+100+100")
+        self.root.geometry("+50+50")
 
         # Create individual sliders for each joint
         controller1 = SliderController(self.root, "Controller 1", "/roboticarm/joint1_position_controller/command", -180, 180, "red")
+        controller2 = SliderController(self.root, "Controller 2", "/roboticarm/joint2_position_controller/command", -90, 90, "orange")
+        controller3 = SliderController(self.root, "Controller 3", "/roboticarm/joint3_position_controller/command", -97.4, 97.4, "yellow")
+        controller4 = SliderController(self.root, "Controller 4", "/roboticarm/joint4_position_controller/command", -97.4, 97.4, "green")
+        controller5 = SliderController(self.root, "Controller 5", "/roboticarm/joint5_position_controller/command", -180, 180, "blue")
+
+        # Create a claw controller
+        claw_controller = ClawController(self.root, "Claw Controller", 0.0, -0.785)
 
     def run(self):
         # Start the Tkinter main loop
